a26e2c5f2c169f7d4ad2f18bb73c9af1d9a2d4e9
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-20 14:07:32 -0500

    dependencies & basic express server

---
+++ 02.0-Contact-Keeper/server.js
@@ -0,0 +1,12 @@
+// This file is essentially the entry point to the back end
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKepper API' })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
