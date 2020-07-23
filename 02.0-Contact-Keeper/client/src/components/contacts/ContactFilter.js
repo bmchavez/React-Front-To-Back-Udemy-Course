9105787f78e750658153ce4b0668a8f92d2b56a8
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-23 14:13:34 -0500

    contact filter and clear state

---
+++ 02.0-Contact-Keeper/client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,35 @@
+// rcf
+import React, { useContext, useRef, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+export default function ContactFilter() {
+  const contactContext = useContext(ContactContext);
+  const text = useRef('');
+
+  const { filterContacts, clearFilter, filtered } = contactContext;
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
+    }
+  });
+
+  const onChange = (e) => {
+    if (text.current.val !== '') {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+
+  return (
+    <form>
+      <input
+        ref={text}
+        type='text'
+        placeholder='Filter Contacts...'
+        onChange={onChange}
+      />
+    </form>
+  );
+}
